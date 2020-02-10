3dc43d7533938b52e0fd53b4e55e854ba6134655
Ramadhan Wiedjayaprakoso <ramadhan.prakoso@datacomm.co.id>
2020-02-10 15:41:34 +0700

    remove comment

--- source/dds/routers/customer.php
+++ source/dds/routers/customer.php
@@ -1,9 +1,4 @@
 <?php 
-
-    // $app->get('/', '\DDSControllers\IndexController:index')->add($request_verify);
-    // $app->get('/customer/', '\DDSControllers\CustomerController:getCustomerList');
-    // $app->get('/insertCustomer', '\DDSControllers\IndexController:phpinfo')->add($request_verify);
-
     //without middleware 
     // $app->group('/customer', function () use ($app) {
     //     $app->get('/', '\DDSControllers\CustomerController:getCustomerList');
